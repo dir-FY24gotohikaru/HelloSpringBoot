a6ec3d677616006f2de29599bb8bd09e4d51ebe4
dir-FY24gotohikaru <hikaru.goto.e884@dirbato.co.jp>
2024-04-24 10:08:05 +0900

    サーバの現在時刻を画面に表示する

--- HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
+++ HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
@@ -1,5 +1,8 @@
 package com.techacademy;
 
+import java.time.LocalDateTime; // 追加
+import java.time.format.DateTimeFormatter; // 追加
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,14 @@ public class HelloSpringBootController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello SpringBootの世界へ!";
+        return "Hello SpringBoot!";
     }
+ // ----- 追加:ここから -----
+    @GetMapping("now")
+    public String dispTime() {
+        String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss") );
+
+        return "現在時刻：" + now;
+    }
+    // ----- 追加:ここまで -----
 }
